3b1fc4c3040cd46b510c0f419bd56396894e6c2c
kkvanonymous <ninja.kunal01@gmail.com>
2021-08-20 10:46:01 +0530

    Initial next tailwind typescript setup
    Signed-off-by: kkvanonymous <ninja.kunal01@gmail.com>

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div>
+      <header className='bg-purple-900 mt-4 mx-3 p-6 rounded shadow-lg'>
+        <h1 className='text-white text-3xl '>NextJS-TailwindCSS Starter</h1>
+      </header>
+    </div>
+  )
+}
